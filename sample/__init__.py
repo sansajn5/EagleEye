82773009456989877c241a8c19f07258e03a3293
Stefan Pacavra <50pace@Stefans-MacBook-Pro.local>
2018-01-23 20:15:41 +0100

    test

--- sample/__init__.py
+++ sample/__init__.py
@@ -1,27 +1,24 @@
-from PyQt5 import QtCore, QtGui, QtWidgets
+import numpy as np
+import cv2
+import sys
+from PyQt5 import QtWidgets
 
-class Ui_Form(object):
-    def setupUi(self, Form):
-        Form.setObjectName("Form")
-        Form.resize(400, 300)
-        self.pushButton = QtWidgets.QPushButton(Form)
-        self.pushButton.setGeometry(QtCore.QRect(70, 50, 75, 23))
-        self.pushButton.setObjectName("pushButton")
-
-        self.retranslateUi(Form)
-        QtCore.QMetaObject.connectSlotsByName(Form)
-
-    def retranslateUi(self, Form):
-        _translate = QtCore.QCoreApplication.translate
-        Form.setWindowTitle(_translate("Form", "Form"))
-        self.pushButton.setText(_translate("Form", "PushButton"))
-
-
-if __name__ == "__main__":
-    import sys
+def window():
     app = QtWidgets.QApplication(sys.argv)
-    Form = QtWidgets.QWidget()
-    ui = Ui_Form()
-    ui.setupUi(Form)
-    Form.show()
+    w = QtWidgets.QWidget()
+    w.setWindowTitle('Trke')
+    w.setGeometry(100,100,700,400)
+    p = QtWidgets.QPushButton(w)
+    p.setText('Play >')
+    p.move(300,350)
+    
+    s = QtWidgets.QPushButton(w)
+    s.setText('Stop x')
+    s.move(380,350)
+    
+    
+    w.show()
     sys.exit(app.exec_())
+
+window()
+
